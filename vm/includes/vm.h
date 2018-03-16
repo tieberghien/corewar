5196bf1ffdfcdc8d3ea1a428b6001798cc7dfbf7
Estelle TIEBERGHIEN <etieberg@e1r7p19.42.fr>
2018-03-16 17:25:41 +0100

    not working (figuring out different steps)

--- vm/includes/vm.h
+++ vm/includes/vm.h
@@ -6,7 +6,7 @@
 /*   By: etieberg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/15 17:44:43 by etieberg          #+#    #+#             */
-/*   Updated: 2018/03/15 17:45:51 by etieberg         ###   ########.fr       */
+/*   Updated: 2018/03/16 17:24:54 by etieberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,13 @@
 # include <limits.h>
 # include "libft.h"
 # include <fcntl.h>
+
+/* dump coded on 32o + id_player between [1-99] */
+
+typedef struct	s_opts
+{
+	int			n_cycles;
+	short		*id_player;
+}				t_opts;
 
 #endif
--- vm/src/main_vm.c
+++ vm/src/main_vm.c
@@ -6,7 +6,7 @@
 /*   By: etieberg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 16:21:37 by etieberg          #+#    #+#             */
-/*   Updated: 2018/03/16 15:49:49 by etieberg         ###   ########.fr       */
+/*   Updated: 2018/03/16 17:24:56 by etieberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,34 @@ static void	display_usage(void)
 
 static void	parse_opts(char **ops)
 {
-
+	return ;
 }
 
 int		main(int ac, char **av)
 {
+	int		i;
 	t_opts	opts;
 	t_champs	*c;
 
+	i = 1;
 	if (ac < 2)
 		display_usage();
-	opts = parse_opts(av + 1);
+	bzero(&opts, sizeof(t_opts));
+	while (i < ac && av[i][0] == '-' && av[i][1] != '\0')
+	{
+		if (av[i][1] == '-')
+		{
+			i++;
+			break ;
+		}
+		parse_options(av[i], &opts);
+		i++;
+	}
 	c = NULL;
 	register_champs(c);
-
+	begin_game(c);
 	while (1)
 	{
-
 		if (!corewar_game())
 			display_result();
 	}
