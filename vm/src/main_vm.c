c79b9b03e720eca80231a2bb07ca91ef3f7ef9a5
Estelle TIEBERGHIEN <etieberg@e3r3p18.42.fr>
2018-03-16 15:59:25 +0100

    skeleton

--- vm/src/main_vm.c
+++ vm/src/main_vm.c
@@ -6,13 +6,38 @@
 /*   By: etieberg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 16:21:37 by etieberg          #+#    #+#             */
-/*   Updated: 2018/03/14 16:23:44 by etieberg         ###   ########.fr       */
+/*   Updated: 2018/03/16 15:49:49 by etieberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
+static void	display_usage(void)
+{
+	return ;
+}
+
+static void	parse_opts(char **ops)
+{
+
+}
+
 int		main(int ac, char **av)
 {
+	t_opts	opts;
+	t_champs	*c;
+
+	if (ac < 2)
+		display_usage();
+	opts = parse_opts(av + 1);
+	c = NULL;
+	register_champs(c);
+
+	while (1)
+	{
+
+		if (!corewar_game())
+			display_result();
+	}
 	return (0);
 }
