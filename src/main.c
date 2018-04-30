2cd8b5ea87fc26b31edd7c7d133fe99626a80acd
Samuel LYNN-EVANS <slynn-ev@e1r12p17.42.fr>
2018-04-30 14:40:16 +0200

    fd 2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: slynn-ev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/08 18:34:20 by slynn-ev          #+#    #+#             */
-/*   Updated: 2018/04/30 13:48:32 by slynn-ev         ###   ########.fr       */
+/*   Updated: 2018/04/30 14:38:55 by slynn-ev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 void	return_invparams(int n, char *type, char *op)
 {
-	ft_putstr_fd("Invalid parameter ", 1);
-	ft_putnbr_fd(n, 1);
-	ft_putstr_fd(" type ", 1);
-	ft_putstr_fd(type, 1);
-	ft_putstr_fd(" for instruction ", 1);
-	ft_putendl_fd(op, 1);
+	ft_putstr_fd("Invalid parameter ", 2);
+	ft_putnbr_fd(n, 2);
+	ft_putstr_fd(" type ", 2);
+	ft_putstr_fd(type, 2);
+	ft_putstr_fd(" for instruction ", 2);
+	ft_putendl_fd(op, 2);
 	exit(0);
 }
 
 void	return_failure(char *str, char *op)
 {
-	ft_putstr_fd(str, 1);
+	ft_putstr_fd(str, 2);
 	if (op != NULL)
-		ft_putendl_fd(op, 1);
+		ft_putendl_fd(op, 2);
 	else
-		ft_putchar_fd('\n', 1);
+		ft_putchar_fd('\n', 2);
 	exit(0);
 }
 
@@ -91,5 +91,7 @@ int		main(int ac, char **av)
 		exit(0);
 	}
 	close(fd);
+	while (1)
+		;
 	return (0);
 }
